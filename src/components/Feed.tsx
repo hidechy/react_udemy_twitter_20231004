e7d77db2d0fc59d953ccd74676789ca3af67f462
toyohide <hide.toyoda@gmail.com>
2023-10-04 19:12:03 +0900

    TweetInput Avatar display

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,9 +1,16 @@
 import { auth } from '../firebase'
+import TweetInput from './TweetInput'
 
 const Feed = () => {
 return (
 <div>Feed
+
+<TweetInput />
+
+<div>
 <button onClick={()=>auth.signOut()}>Logout</button>
+</div>
+
 </div>
 )
 }
